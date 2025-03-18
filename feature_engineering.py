c0b1ea7098425128090bd2043c3d163ad2ae2dc9
Patrice <fangying197711@MacBook-Pro-5.local>
2025-03-18 19:09:36 +0800

    Edited by Andrew

--- feature_engineering.py
+++ feature_engineering.py
@@ -15,7 +15,7 @@ from ta.others import DailyLogReturnIndicator, CumulativeReturnIndicator
 class FeatureEngineer:
     """Feature engineering class for creating technical indicators and other features."""
     def __init__(self):
-        self.windows = [8, 48, 96]
+        self.windows = [4,8, 48, 96,480, 960]
 
     def trend_indicators(self, df):
         """Create trend indicators."""
@@ -28,6 +28,19 @@ class FeatureEngineer:
         df['macd'] = macd.macd()
         df['macd_signal'] = macd.macd_signal()
         df['macd_diff'] = macd.macd_diff()
+        #andrew
+        # Aroon Indicator - Common window: 25
+        aroon = AroonIndicator(high=df['high'],low=df['low'], window=24)
+        df['aroon_up_24'] = aroon.aroon_up()
+        df['aroon_down_24'] = aroon.aroon_down()
+        df['aroon_indicator_24'] = aroon.aroon_indicator()
+
+
+        # 9. Commodity Channel Index (CCI) - Common window: 20
+        df['cci_24'] = CCIIndicator(high=df['high'], low=df['low'], close=df['close'], window=24).cci()
+
+        #andrew
+
         return df
 
     def momentum_indicators(self, df):
@@ -35,6 +48,35 @@ class FeatureEngineer:
         for length in self.windows:
             # 7. Relative Strength Index (RSI)
             df[f'rsi_{length}'] = RSIIndicator(close=df['close'], window=length).rsi()
+
+        # andrew
+        #Williams % R
+        df[f'williamsr_{8}'] = WilliamsRIndicator(high=df['high'], low=df['low'], close=df['close'],
+                                                           lbp=8).williams_r()
+
+        df[f'williamsr_{24}'] = WilliamsRIndicator(high=df['high'], low=df['low'], close=df['close'],
+                                                  lbp=96).williams_r()
+
+        df[f'williamsr_{48}'] = WilliamsRIndicator(high=df['high'], low=df['low'], close=df['close'],
+                                                  lbp=96).williams_r()
+
+        df[f'williamsr_{96}'] = WilliamsRIndicator(high=df['high'], low=df['low'], close=df['close'],
+                                                  lbp=96).williams_r()
+
+        # Stochastic RSI - Common window: 14, smoothed with 3 periods
+        stoch_rsi = StochRSIIndicator(close=df['close'], window=16, smooth1=3, smooth2=3)
+        df['stoch_rsi_16'] = stoch_rsi.stochrsi()
+        df['stoch_rsi_k_16'] = stoch_rsi.stochrsi_k()
+        df['stoch_rsi_d_16'] = stoch_rsi.stochrsi_d()
+
+        # True Strength Index (TSI) - Common slow=25, fast=13
+        df['tsi_24_12'] = TSIIndicator(close=df['close'], window_slow=24, window_fast=12).tsi()
+
+        # Awesome Oscillator - Uses (5, 34) as default
+        df['awesome_oscillator'] = AwesomeOscillatorIndicator(high=df['high'], low=df['low'], window1=4,
+                                                              window2=32).awesome_oscillator()
+
+        # andrew
         return df
 
     def volatility_indicators(self, df):
@@ -53,12 +95,19 @@ class FeatureEngineer:
             dc = DonchianChannel(high=df['high'], low=df['low'], close=df['close'], window=length)
             df[f'donchian_high_{length}'] = dc.donchian_channel_hband()
             df[f'donchian_low_{length}'] = dc.donchian_channel_lband()
+
+
         return df
 
     def volume_indicators(self, df):
         """Create volume indicators."""
         # 13. On-Balance Volume (OBV)
         df['obv'] = OnBalanceVolumeIndicator(close=df['close'], volume=df['volume']).on_balance_volume()
+        # andrew
+        # 好像挺有用的
+        for length in [4*2,4*24]:
+            df[f'vollume_{length}p_quantile']=df['volume'].rolling(length,min_periods=int(length/2)).apply(lambda x:x.rank(pct=True).iloc[-1],raw=False)
+        # andrew
         return df
 
     def custom_oscilators(self, df):
@@ -84,6 +133,18 @@ class FeatureEngineer:
         df['Month'] = df['Open time'].dt.month
         df['Day'] = df['Open time'].dt.day
         df['Open hour'] = df['Open time'].dt.hour
+
+
+        # andrew
+        # astype categoryy也会跑不动
+        # df['Year'] = df['Year'].astype('category')
+        # df['Month'] = df['Month'].astype('category')
+        # df['Day'] = df['Day'].astype('category')
+        # df['Open hour'] = df['Open hour'].astype('category')
+        #
+        # df['Weekday'] = df['Weekday'].astype('category')
+        # andrew
+
         return df
 
     def process_data(self, df):
